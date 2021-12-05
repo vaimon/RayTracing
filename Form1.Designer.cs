001102a1fe3bdca981658a16cc3f9fcb5b356078
Andrew Vaimon <niko142000@mail.ru>
2021-12-05 13:12:46 +0300

    i don't remember what is that

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,10 +29,17 @@ namespace RayTracing
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1001, 600);
+            this.Name = "Form1";
+            this.Text = "Корнуэльская комната";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
