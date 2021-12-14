caa272ffbd0e089b02074641bfa1346b77185f1d
Andrew Vaimon <niko142000@mail.ru>
2021-12-14 10:15:39 +0300

    selector

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,11 +32,16 @@ namespace RayTracing
             this.progressBar = new System.Windows.Forms.ProgressBar();
             this.btnCompute = new System.Windows.Forms.Button();
             this.labelTime = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
             this.SuspendLayout();
             // 
             // progressBar
             // 
-            this.progressBar.Location = new System.Drawing.Point(166, 544);
+            this.progressBar.Location = new System.Drawing.Point(163, 457);
             this.progressBar.Name = "progressBar";
             this.progressBar.Size = new System.Drawing.Size(598, 44);
             this.progressBar.TabIndex = 0;
@@ -44,7 +49,7 @@ namespace RayTracing
             // 
             // btnCompute
             // 
-            this.btnCompute.Location = new System.Drawing.Point(12, 544);
+            this.btnCompute.Location = new System.Drawing.Point(9, 457);
             this.btnCompute.Name = "btnCompute";
             this.btnCompute.Size = new System.Drawing.Size(148, 44);
             this.btnCompute.TabIndex = 1;
@@ -55,23 +60,57 @@ namespace RayTracing
             // labelTime
             // 
             this.labelTime.AutoSize = true;
-            this.labelTime.Location = new System.Drawing.Point(770, 554);
+            this.labelTime.Location = new System.Drawing.Point(767, 467);
             this.labelTime.Name = "labelTime";
             this.labelTime.Size = new System.Drawing.Size(68, 25);
             this.labelTime.TabIndex = 2;
             this.labelTime.Text = "Время:";
             this.labelTime.Visible = false;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.canvas);
+            this.groupBox1.Location = new System.Drawing.Point(9, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(593, 439);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Сцена";
+            // 
+            // canvas
+            // 
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(3, 27);
+            this.canvas.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(587, 409);
+            this.canvas.TabIndex = 10;
+            this.canvas.TabStop = false;
+            this.canvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(690, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1001, 600);
+            this.ClientSize = new System.Drawing.Size(1001, 518);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.labelTime);
             this.Controls.Add(this.btnCompute);
             this.Controls.Add(this.progressBar);
             this.Name = "Form1";
             this.Text = "Корнуэльская комната";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,6 +121,9 @@ namespace RayTracing
         private System.Windows.Forms.ProgressBar progressBar;
         private System.Windows.Forms.Button btnCompute;
         private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,12 +10,28 @@ using System.Windows.Forms;
 
 namespace RayTracing
 {
+    public enum SelectedItem { BackWall, RightWall, LeftWall, Cube, SphereOnCube, SphereOnGround}
     public partial class Form1 : Form
     {
         RayTracing rayTracing;
+
+        Rectangle backWall;
+        Rectangle cube;
+        Rectangle sphereOnCube;
+        Rectangle sphereOnGround;
+        
+
+        Pen standardPen = new Pen(Color.Black, 2);
+        Pen highlightPen = new Pen(Color.DarkRed, 4);
+        Graphics g;
+
+        SelectedItem currentItem;
+
         public Form1()
         {
             InitializeComponent();
+            canvas.Image = new Bitmap(canvas.Width,canvas.Height);
+            g = Graphics.FromImage(canvas.Image);
             rayTracing = new RayTracing(new LightSource(new Point(0,10,10),1.5), new Room(new Point(0,0,9),20, Color.Gray));
             rayTracing.renderProgress += updateProgress;
             rayTracing.addShape(new Sphere(new Point(5, -8, 17), 4, Color.DarkSalmon));
@@ -26,6 +42,64 @@ namespace RayTracing
             //rayTracing.addShape(new Face(new Point(0, 0, 6), new Vector(0, 0, -1), new Vector(0, 1, 0), 4, 4, Color.DarkRed));
             //rayTracing.addShape(new Sphere(new Point(0, 0, 6), 2, Color.DarkRed));
             //rayTracing.addShape(new Sphere(new Point(7, 5, 18), 4, Color.DarkBlue));
+
+            backWall = new Rectangle(canvas.Width / 2 - 100, canvas.Height / 2 - 100, 200, 200);
+            cube = new Rectangle(canvas.Width / 2 - 125, canvas.Height / 2 + 25, 100, 100);
+            sphereOnCube = new Rectangle(canvas.Width / 2 - 100, canvas.Height / 2 - 25, 50, 50);
+            sphereOnGround = new Rectangle(canvas.Width / 2 + 25, canvas.Height / 2 + 40, 100, 100);
+            currentItem = SelectedItem.BackWall;
+            redrawScheme();
+        }
+
+        
+
+        void redrawScheme()
+        {
+            g.Clear(SystemColors.Control);
+            Pen backwallPen = standardPen, rightwallPen = standardPen, leftwallPen = standardPen, sphereOnCubePen = standardPen, sphereOnGroundPen = standardPen, cubePen = standardPen;
+            switch (currentItem)
+            {
+                case SelectedItem.BackWall: backwallPen = highlightPen; break;
+                case SelectedItem.Cube: cubePen = highlightPen; break;
+                case SelectedItem.LeftWall: leftwallPen = highlightPen; break;
+                case SelectedItem.RightWall: rightwallPen = highlightPen; break;
+                case SelectedItem.SphereOnCube: sphereOnCubePen = highlightPen; break;
+                case SelectedItem.SphereOnGround: sphereOnGroundPen = highlightPen; break;
+            }
+            
+            var shapeBrush = new SolidBrush(SystemColors.Control);
+
+            g.DrawLine(backwallPen, backWall.Location, new System.Drawing.Point(backWall.Location.X + backWall.Width, backWall.Location.Y));
+            g.DrawLine(backwallPen, backWall.Location, new System.Drawing.Point(backWall.Location.X, backWall.Location.Y + backWall.Height));
+            g.DrawLine(backwallPen, new System.Drawing.Point(backWall.Location.X + backWall.Width, backWall.Location.Y), new System.Drawing.Point(backWall.Location.X + backWall.Width, backWall.Location.Y + backWall.Height));
+            g.DrawLine(backwallPen, new System.Drawing.Point(backWall.Location.X, backWall.Location.Y + backWall.Height), new System.Drawing.Point(backWall.Location.X + backWall.Width, backWall.Location.Y + backWall.Height));
+
+            g.DrawLine(leftwallPen, backWall.Location, new System.Drawing.Point(0,0));
+            g.DrawLine(leftwallPen, new System.Drawing.Point(backWall.Location.X, backWall.Location.Y + backWall.Height), new System.Drawing.Point(0, canvas.Height));
+            if(currentItem == SelectedItem.LeftWall)
+            {
+                g.DrawLine(leftwallPen, backWall.Location, new System.Drawing.Point(backWall.Location.X, backWall.Location.Y + backWall.Height));
+            }
+
+            g.DrawLine(rightwallPen, new System.Drawing.Point(backWall.Location.X + backWall.Width, backWall.Location.Y), new System.Drawing.Point(canvas.Width,0));
+            g.DrawLine(rightwallPen, new System.Drawing.Point(backWall.Location.X + backWall.Width, backWall.Location.Y + backWall.Height), new System.Drawing.Point(canvas.Width, canvas.Height));
+            if(currentItem == SelectedItem.RightWall)
+            {
+                g.DrawLine(rightwallPen, new System.Drawing.Point(backWall.Location.X + backWall.Width, backWall.Location.Y), new System.Drawing.Point(backWall.Location.X + backWall.Width, backWall.Location.Y + backWall.Height));
+            }
+
+            g.FillRectangle(shapeBrush, cube);
+            g.DrawRectangle(cubePen, cube);
+
+            g.FillEllipse(shapeBrush, sphereOnCube);
+            g.DrawEllipse(sphereOnCubePen, sphereOnCube);
+
+            g.FillEllipse(shapeBrush, sphereOnGround);
+            g.DrawEllipse(sphereOnGroundPen, sphereOnGround);
+
+
+
+            canvas.Invalidate();
         }
 
         // Спасибо лабе по ИС за реализацию такой же штуки
@@ -58,6 +132,32 @@ namespace RayTracing
             {
                 labelTime.Text = "HAHA exception";
             }
+        }
+
+        private void canvas_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (cube.Contains(e.Location))
+            {
+                currentItem = SelectedItem.Cube;
+            }else if (sphereOnCube.Contains(e.Location))
+            {
+                currentItem = SelectedItem.SphereOnCube;
+            } else if (sphereOnGround.Contains(e.Location))
+            {
+                currentItem = SelectedItem.SphereOnGround;
+            } else if (backWall.Contains(e.Location))
+            {
+                currentItem = SelectedItem.BackWall;
+            }else if(e.Location.X < backWall.Location.X)
+            {
+                currentItem = SelectedItem.LeftWall;
+            }
+            else if (e.Location.X > backWall.Location.X + backWall.Width)
+            {
+                currentItem = SelectedItem.RightWall;
+            }
+
+            redrawScheme();
         }
     }
 }
